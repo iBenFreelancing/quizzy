1d2a1778f318b63efb1357fc333f511d86bf6e02
iBen <ibenfreelancing@yahoo.com>
2023-01-06 05:58:27 +0100

    done with answering MCQ

---
+++ Js/q.js
@@ -0,0 +1,442 @@
+var Q = [
+    {
+        q: "which of the following is not a JavasSript data type?",
+        o1: "String",
+        o2: "Logic",
+        o3: "Number",
+        o4: "script",
+        ans: ['o2','o4']
+    },
+    {
+        q: "Who made the dog bark?",
+        o1: "woo",
+        o2: "whoo",
+        o3: "whoop",
+        o4: "hoo",
+        ans: ['o2']
+    },
+    {
+        q: "why y?",
+        o1: "because it has 3 branches",
+        o2: "because it is near z",
+        o3: "is just a convention",
+        o4: "can't answer this, this is no valid question",
+        ans: ['o3']
+    },
+    {
+        q: "Why did the water droplets changed the weather",
+        o1: "hahahah, too easy, but am not in the mood to answer this",
+        o2: "they were calling for help from the sky",
+        o3: "it was just a child's play",
+        o4: "They didn't change anything, is plure scanda",
+        ans: ['o2']
+    },
+    {
+        q: "How did Cyndy know she was happy",
+        o1: "she used the mirror",
+        o2: "she saw her neighbour's faces",
+        o3: "she only has one mood and that is happy",
+        o4: "her heart beat in a special was, she was full of emotions and her mind was in a that she wished she stayed there forever. that certainly was the hint",
+        ans: ['o4']
+    },
+    {
+        q: "What can you say about my beautiful Anabelli",
+        o1: "She's a sweetheart to every man",
+        o2: "her simplicity and deligthfulness is a trap that captures every heart that is capable of love",
+        o3: "she used to be gorgeous before she turned down my proposal. Now she's just a rotten tommato",
+        o4: "Ana has belli",
+        ans: ['o1','o2']
+    },
+    {
+        q: "Which of the following is a JavaScript declearation keyword?",
+        o1: "const",
+        o2: "let",
+        o3: "vars",
+        o4: "function",
+        ans: ['o1','o2','o4']
+    },
+    {
+        q: "What will the code below return? function (){return void}",
+        o1: "undefined",
+        o2: "null",
+        o3: "err",
+        o4: "the function itself",
+        ans: ['o2']
+    },
+    {
+        q: "what will be logged onto the console. { let x = 3; function twist() {(x=5); x += 2;} console.log(x--)}",
+        o1: "3",
+        o2: "2",
+        o3: "7",
+        o4: "6",
+        ans: ['o2']
+    },
+    {
+        q: "How do you select an element with id 'txt' using vanilla JavaScript? ",
+        o1: "document.getElementById('txt')",
+        o2: "document.getElementByAttribute(id = 'txt')",
+        o3: "DOM.getElementByAttribute(id = 'txt')",
+        o4: "document.querySelector('#txt')",
+        ans: ['o1','o4']
+    },
+    {
+        q: "which of the following is not a JavasSript data type?",
+        o1: "String",
+        o2: "Logic",
+        o3: "Number",
+        o4: "script",
+        ans: ['o2','o4']
+    },
+    {
+        q: "Which of the following is a JavaScript declearation keyword?",
+        o1: "const",
+        o2: "let",
+        o3: "vars",
+        o4: "function",
+        ans: ['o1','o2','o4']
+    },
+    {
+        q: "What will the code below return? function (){return void}",
+        o1: "undefined",
+        o2: "null",
+        o3: "err",
+        o4: "the function itself",
+        ans: ['o2']
+    },
+    {
+        q: "what will be logged onto the console. { let x = 3; function twist() {(x=5); x += 2;} console.log(x--)}",
+        o1: "3",
+        o2: "2",
+        o3: "7",
+        o4: "6",
+        ans: ['o2']
+    },
+    {
+        q: "How do you select an element with id 'txt' using vanilla JavaScript? ",
+        o1: "document.getElementById('txt')",
+        o2: "document.getElementByAttribute(id = 'txt')",
+        o3: "DOM.getElementByAttribute(id = 'txt')",
+        o4: "document.querySelector('#txt')",
+        ans: ['o1','o4']
+    },
+    {
+        q: "which of the following is not a JavasSript data type?",
+        o1: "String",
+        o2: "Logic",
+        o3: "Number",
+        o4: "script",
+        ans: ['o2','o4']
+    },
+    {
+        q: "Which of the following is a JavaScript declearation keyword?",
+        o1: "const",
+        o2: "let",
+        o3: "vars",
+        o4: "function",
+        ans: ['o1','o2','o4']
+    },
+    {
+        q: "What will the code below return? function (){return void}",
+        o1: "undefined",
+        o2: "null",
+        o3: "err",
+        o4: "the function itself",
+        ans: ['o2']
+    },
+    {
+        q: "what will be logged onto the console. { let x = 3; function twist() {(x=5); x += 2;} console.log(x--)}",
+        o1: "3",
+        o2: "2",
+        o3: "7",
+        o4: "6",
+        ans: ['o2']
+    },
+    {
+        q: "How do you select an element with id 'txt' using vanilla JavaScript? ",
+        o1: "document.getElementById('txt')",
+        o2: "document.getElementByAttribute(id = 'txt')",
+        o3: "DOM.getElementByAttribute(id = 'txt')",
+        o4: "document.querySelector('#txt')",
+        ans: ['o1','o4']
+    },
+    {
+        q: "which of the following is not a JavasSript data type?",
+        o1: "String",
+        o2: "Logic",
+        o3: "Number",
+        o4: "script",
+        ans: ['o2','o4']
+    },
+    {
+        q: "Which of the following is a JavaScript declearation keyword?",
+        o1: "const",
+        o2: "let",
+        o3: "vars",
+        o4: "function",
+        ans: ['o1','o2','o4']
+    },
+    {
+        q: "What will the code below return? function (){return void}",
+        o1: "undefined",
+        o2: "null",
+        o3: "err",
+        o4: "the function itself",
+        ans: ['o2']
+    },
+    {
+        q: "what will be logged onto the console. { let x = 3; function twist() {(x=5); x += 2;} console.log(x--)}",
+        o1: "3",
+        o2: "2",
+        o3: "7",
+        o4: "6",
+        ans: ['o2']
+    },
+    {
+        q: "How do you select an element with id 'txt' using vanilla JavaScript? ",
+        o1: "document.getElementById('txt')",
+        o2: "document.getElementByAttribute(id = 'txt')",
+        o3: "DOM.getElementByAttribute(id = 'txt')",
+        o4: "document.querySelector('#txt')",
+        ans: ['o1','o4']
+    },
+    {
+        q: "which of the following is not a JavasSript data type?",
+        o1: "String",
+        o2: "Logic",
+        o3: "Number",
+        o4: "script",
+        ans: ['o2','o4']
+    },
+    {
+        q: "Which of the following is a JavaScript declearation keyword?",
+        o1: "const",
+        o2: "let",
+        o3: "vars",
+        o4: "function",
+        ans: ['o1','o2','o4']
+    },
+    {
+        q: "What will the code below return? function (){return void}",
+        o1: "undefined",
+        o2: "null",
+        o3: "err",
+        o4: "the function itself",
+        ans: ['o2']
+    },
+    {
+        q: "what will be logged onto the console. { let x = 3; function twist() {(x=5); x += 2;} console.log(x--)}",
+        o1: "3",
+        o2: "2",
+        o3: "7",
+        o4: "6",
+        ans: ['o2']
+    },
+    {
+        q: "How do you select an element with id 'txt' using vanilla JavaScript? ",
+        o1: "document.getElementById('txt')",
+        o2: "document.getElementByAttribute(id = 'txt')",
+        o3: "DOM.getElementByAttribute(id = 'txt')",
+        o4: "document.querySelector('#txt')",
+        ans: ['o1','o4']
+    },
+    {
+        q: "which of the following is not a JavasSript data type?",
+        o1: "String",
+        o2: "Logic",
+        o3: "Number",
+        o4: "script",
+        ans: ['o2','o4']
+    },
+    {
+        q: "Which of the following is a JavaScript declearation keyword?",
+        o1: "const",
+        o2: "let",
+        o3: "vars",
+        o4: "function",
+        ans: ['o1','o2','o4']
+    },
+    {
+        q: "What will the code below return? function (){return void}",
+        o1: "undefined",
+        o2: "null",
+        o3: "err",
+        o4: "the function itself",
+        ans: ['o2']
+    },
+    {
+        q: "what will be logged onto the console. { let x = 3; function twist() {(x=5); x += 2;} console.log(x--)}",
+        o1: "3",
+        o2: "2",
+        o3: "7",
+        o4: "6",
+        ans: ['o2']
+    },
+    {
+        q: "How do you select an element with id 'txt' using vanilla JavaScript? ",
+        o1: "document.getElementById('txt')",
+        o2: "document.getElementByAttribute(id = 'txt')",
+        o3: "DOM.getElementByAttribute(id = 'txt')",
+        o4: "document.querySelector('#txt')",
+        ans: ['o1','o4']
+    },
+    {
+        q: "which of the following is not a JavasSript data type?",
+        o1: "String",
+        o2: "Logic",
+        o3: "Number",
+        o4: "script",
+        ans: ['o2','o4']
+    },
+    {
+        q: "Which of the following is a JavaScript declearation keyword?",
+        o1: "const",
+        o2: "let",
+        o3: "vars",
+        o4: "function",
+        ans: ['o1','o2','o4']
+    },
+    {
+        q: "What will the code below return? function (){return void}",
+        o1: "undefined",
+        o2: "null",
+        o3: "err",
+        o4: "the function itself",
+        ans: ['o2']
+    },
+    {
+        q: "what will be logged onto the console. { let x = 3; function twist() {(x=5); x += 2;} console.log(x--)}",
+        o1: "3",
+        o2: "2",
+        o3: "7",
+        o4: "6",
+        ans: ['o2']
+    },
+    {
+        q: "How do you select an element with id 'txt' using vanilla JavaScript? ",
+        o1: "document.getElementById('txt')",
+        o2: "document.getElementByAttribute(id = 'txt')",
+        o3: "DOM.getElementByAttribute(id = 'txt')",
+        o4: "document.querySelector('#txt')",
+        ans: ['o1','o4']
+    },
+    {
+        q: "which of the following is not a JavasSript data type?",
+        o1: "String",
+        o2: "Logic",
+        o3: "Number",
+        o4: "script",
+        ans: ['o2','o4']
+    },
+    {
+        q: "Which of the following is a JavaScript declearation keyword?",
+        o1: "const",
+        o2: "let",
+        o3: "vars",
+        o4: "function",
+        ans: ['o1','o2','o4']
+    },
+    {
+        q: "What will the code below return? function (){return void}",
+        o1: "undefined",
+        o2: "null",
+        o3: "err",
+        o4: "the function itself",
+        ans: ['o2']
+    },
+    {
+        q: "what will be logged onto the console. { let x = 3; function twist() {(x=5); x += 2;} console.log(x--)}",
+        o1: "3",
+        o2: "2",
+        o3: "7",
+        o4: "6",
+        ans: ['o2']
+    },
+    {
+        q: "How do you select an element with id 'txt' using vanilla JavaScript? ",
+        o1: "document.getElementById('txt')",
+        o2: "document.getElementByAttribute(id = 'txt')",
+        o3: "DOM.getElementByAttribute(id = 'txt')",
+        o4: "document.querySelector('#txt')",
+        ans: ['o1','o4']
+    },
+    {
+        q: "which of the following is not a JavasSript data type?",
+        o1: "String",
+        o2: "Logic",
+        o3: "Number",
+        o4: "script",
+        ans: ['o2','o4']
+    },
+    {
+        q: "Which of the following is a JavaScript declearation keyword?",
+        o1: "const",
+        o2: "let",
+        o3: "vars",
+        o4: "function",
+        ans: ['o1','o2','o4']
+    },
+    {
+        q: "What will the code below return? function (){return void}",
+        o1: "undefined",
+        o2: "null",
+        o3: "err",
+        o4: "the function itself",
+        ans: ['o2']
+    },
+    {
+        q: "what will be logged onto the console. { let x = 3; function twist() {(x=5); x += 2;} console.log(x--)}",
+        o1: "3",
+        o2: "2",
+        o3: "7",
+        o4: "6",
+        ans: ['o2']
+    },
+    {
+        q: "How do you select an element with id 'txt' using vanilla JavaScript? ",
+        o1: "document.getElementById('txt')",
+        o2: "document.getElementByAttribute(id = 'txt')",
+        o3: "DOM.getElementByAttribute(id = 'txt')",
+        o4: "document.querySelector('#txt')",
+        ans: ['o1','o4']
+    },
+    {
+        q: "which of the following is not a JavasSript data type?",
+        o1: "String",
+        o2: "Logic",
+        o3: "Number",
+        o4: "script",
+        ans: ['o2','o4']
+    },
+    {
+        q: "Which of the following is a JavaScript declearation keyword?",
+        o1: "const",
+        o2: "let",
+        o3: "vars",
+        o4: "function",
+        ans: ['o1','o2','o4']
+    },
+    {
+        q: "What will the code below return? function (){return void}",
+        o1: "undefined",
+        o2: "null",
+        o3: "err",
+        o4: "the function itself",
+        ans: ['o2']
+    },
+    {
+        q: "what will be logged onto the console. { let x = 3; function twist() {(x=5); x += 2;} console.log(x--)}",
+        o1: "3",
+        o2: "2",
+        o3: "7",
+        o4: "6",
+        ans: ['o2']
+    },
+    {
+        q: "How do you select an element with id 'txt' using vanilla JavaScript? ",
+        o1: "document.getElementById('txt')",
+        o2: "document.getElementByAttribute(id = 'txt')",
+        o3: "DOM.getElementByAttribute(id = 'txt')",
+        o4: "document.querySelector('#txt')",
+        ans: ['o1','o4']
+    }
+];
--- Js/quizz.js
+++ Js/quizz.js
@@ -1,45 +1,3 @@
-var Q = [
-    {
-    q: "which of the following is not a JavasSript data type?",
-    o1: "String",
-    o2: "Logic",
-    o3: "Number",
-    o4: "script",
-    ans: ['o2','o4']
-},
-{
-    q: "Which of the following is a JavaScript declearation keyword?",
-    o1: "const",
-    o2: "let",
-    o3: "vars",
-    o4: "function",
-    ans: ['o1','o2','o4']
-},
-{
-    q: "What will the code below return? function (){return void}",
-    o1: "undefined",
-    o2: "null",
-    o3: "err",
-    o4: "the function itself",
-    ans: ['o2']
-},
-{
-    q: "what will be logged onto the console. { let x = 3; function twist() {(x=5); x += 2;} console.log(x--)}",
-    o1: "3",
-    o2: "2",
-    o3: "7",
-    o4: "6",
-    ans: ['o2']
-},
-{
-    q: "How do you select an element with id 'txt' using vanilla JavaScript? ",
-    o1: "document.getElementById('txt')",
-    o2: "document.getElementByAttribute(id = 'txt')",
-    o3: "DOM.getElementByAttribute(id = 'txt')",
-    o4: "document.querySelector('#txt')",
-    ans: ['o1','o4']
-}
-];
 var qo = {
     quizz: "question",
     o1: "option A",
@@ -48,15 +6,19 @@ var qo = {
     o4: "option D",
     ans: ['o2']
 };
-
+var hider = 'hideit';
 // bg: array of objects {id:'id', cl:'class added'}
 class Test {
     constructor(){
-        this.data = {
-            q: [], // array of questions that user attempted
-            currentQuizz: {id:-1, ans:[], answered: false, userChoice: [], bg:[]},
+        this.allQuizz = [];
+        this.currentQuizz = {id:'', ans:[], answered: false, userChoice: [], formats:[]};
+        this.lastQuizzIndex = -1;
+        this.data = {attempt: 0, pass: 0, fail: 0};
 
-        }
+        this.on = false;
+        this.first = true;
+        this.quizzInfoOn = false;
+        this.oldQuizz = false;
 
         this.quizz = document.querySelector('#qz');
         this.o1 = document.querySelector('#qzo1');
@@ -74,42 +36,89 @@ class Test {
 
     events = () => {
         let that = this;
-        this.btno1.addEventListener('click', function(){
-           that.setUserChoice([this.id.slice(4)]);
-        })
+        this.btno1.addEventListener('click', function(){that.analyseAnswer(this.id.slice(4));})
+        this.btno2.addEventListener('click', function(){that.analyseAnswer(this.id.slice(4));})
+        this.btno3.addEventListener('click', function(){that.analyseAnswer(this.id.slice(4));})
+        this.btno4.addEventListener('click', function(){that.analyseAnswer(this.id.slice(4));})
     }
 
-    setQuizz = (qz) => {
+    start = () => {
+       
+        if(this.on){
+            //stop test
+            //reset everything   
+            alert('text don already start since') ;
+            return
+        }
+
+        this.on == false?true:false;
+        this.quizzManager();
+        
+    }
+
+    quizzManager = () => {
+
+        let newIndex = this.setNewQIndex();
+        if(newIndex === false){
+            alert('No quizz, perhaps we have reached the end')
+            return
+        }
+        
+        this.saveAnswer();
+        this.clearFormats(this.currentQuizz.formats);
+        this.clearCurrentQuizz();
+        let qz = this.#setQuizz(newIndex);
+        this.setFomats([... qz.ans], 'right');
+        this.loadQuizz(qz);
+        this.#createBtn(newIndex+1);
+        this.liveUpdate();        
+    }
+
+    clearCurrentQuizz = () => {
+        this.currentQuizz = {id:'', ans:[], answered: false, userChoice: [], formats:[]};
+    }
+
+    setNewQIndex = () => {
+        if(this.first){ this.first = false; return 0}
+        if((this.lastQuizzIndex+1) >= Q.length){return false}
+        this.oldQuizz = false;
+        if(this.quizzInfoOn){
+            this.quizzInfoOn = false;
+            document.querySelector('#quizzDetails').classList.add(hider);        
+        }
+        return (this.lastQuizzIndex + 1)
+    }
+
+    #setQuizz = (quizzIndex) => {
+        let newQuizz = {... Q[quizzIndex]};
+        this.lastQuizzIndex += 1;
+        this.currentQuizz.id = this.lastQuizzIndex;
+        this.currentQuizz.ans = [... newQuizz.ans];
+        this.oldQuizz = false;
+        return newQuizz
+    }
+
+    setFomats = (fm, v) => {
+        for(let c of fm){this.currentQuizz.formats.push({id: 'btn'+c, cl: v});}
+    }
+
+    loadQuizz = (qz) => {
         this.quizz.innerHTML = qz.q;
         this.o1.innerHTML = qz.o1;
         this.o2.innerHTML = qz.o2;
         this.o3.innerHTML = qz.o3;
         this.o4.innerHTML = qz.o4;
     }
-    setColors = (bool, l) => {
-        if(!bool) {return}
-        if(l.length <= 0) {return}
-        for(let c of l){
-            this[c.id].classList.add(c.cl)
+
+    loadFormat = () => {
+        if(this.currentQuizz.formats.length <= 0) {return}
+        for(let f of this.currentQuizz.formats){
+            this[f.id].classList.add(f.cl)
            // this.data.currentQuizz.bg.push({id: c.id, cl: c.cl});
         }
     }
 
-    clearColors = (l) => {
-        if(l.length <=0){return}
-       lg(l)
-        for(let c of l ){
-           this[c.id].classList.remove(c.cl);
-        }
-        this.data.currentQuizz.bg = [];
-    }
-
-    getQuizz = (nextIndex) => {
-        if(nextIndex >= Q.length) {return false}
-        return Q[nextIndex]
-    }
-
-    createBtn = (id) => {
+    #createBtn = (id) => {
         let btn = document.createElement('button');
         btn.id = "navBtn"+id;
         btn.innerText = id;
@@ -117,66 +126,160 @@ class Test {
         document.querySelector('#hScroll').appendChild(btn);
     }
 
-    loadNewQuizz = () => {
-        let newIndex = this.data.currentQuizz.id + 1;
-        let newQuizz = this.getQuizz(newIndex);
-        if(!newQuizz){alert('No further question'); return}
-        this.data.q.push({... this.data.currentQuizz}); 
-        this.data.currentQuizz.id += 1; 
-        this.data.currentQuizz.ans = [... newQuizz.ans]; 
-        this.clearColors(this.data.currentQuizz.bg);
-        this.setQuizz(newQuizz);
-        for(let c of newQuizz.ans){
-            this.data.currentQuizz.bg.push({id: 'btn'+ c,cl: 'right'} )
-        }
+    liveUpdate = () => {
+        document.querySelector('#qCount').innerText = (this.currentQuizz.id +1) + " of " + Q.length;
+    }
 
-        //this.setColors(true, this.data.currentQuizz.bg);
-        document.querySelector('#qCount').innerText = (newIndex+1) + " of " + Q.length;
-        this.createBtn(newIndex+1);
+    loadLiveUpdate = () => {
+
+    }
+
+    analyseAnswer = (ans) => {
+        if(this.currentQuizz.answered){return}
+        this.currentQuizz.userChoice = [... [ans]];
+        this.currentQuizz.answered = true;
+        let btnHint = document.querySelector('#navBtn'+ (this.currentQuizz.id+1));
+        btnHint.scrollIntoView({behavior:'smooth', inline:'center', block:'center'});
+        this.data.attempt +=1  
+        if(this.currentQuizz.ans.indexOf(ans) < 0){ 
+            this.setFomats([ans], 'wrong');
+            btnHint.classList.add('wrong');
+            this.data.fail +=1  
+        }else {
+             btnHint.classList.add('right');           
+             this.data.pass +=1             
+        }
+        if(this.oldQuizz){
+            let nextOldQuizzIndex = this.updateQuizz();
+            if(nextOldQuizzIndex === false) { this.quizzManager()}
+            else{this.loadOldQuizz(nextOldQuizzIndex+1)}
+        }
+        else{
+            this.saveAnswer(); this.quizzManager();
+        }
+    }
+
+    saveAnswer = () => {
+      
+        if(parseInt(this.currentQuizz.id) === this.allQuizz.length){
+            lg('we are pushing it');
+            this.allQuizz.push({... this.currentQuizz});    
+        }
+       // lg(this.allQuizz)
+    }
+
+    updateQuizz = () => {
+        let idx = this.currentQuizz.id;
+        this.allQuizz[idx].formats = [... this.currentQuizz.formats];
+        this.allQuizz[idx].answered = true;
+        //this.clearFormats(this.currentQuizz.formats);
+        
+        let nextIdx = false;
+        for(let i = idx; i< this.allQuizz.length; i++){
+            if(this.allQuizz[i].answered === false){
+                nextIdx = i; break;
+            }
+        }
+        return nextIdx
+    }
+
+    clearFormats = (formats) => {
+        if(formats.length <=0){return}
+        for(let c of formats ){
+           this[c.id].classList.remove(c.cl);
+        }
+        //this.currentQuizz.formats = [];
     }
 
     loadOldQuizz = (oldID) => {
-        //get old quizz id
-        let oldq = this.data.q[oldID-1];
-        // clear current formating
-        this.clearColors(this.data.currentQuizz.bg);
-        //load old quizz
-        this.setQuizz(Q[oldID-1]);
-        if(!oldq.answered){return}
-       
-       // this.data.currentQuizz.bg = [... oldq.bg];
-       // this.data.currentQuizz.ans = [... oldq.ans];
-       // this.data.currentQuizz.answered = oldq.answered;
-        this.setColors(true, oldq.bg)
-        
-    }
+        oldID--;
+       // let oldQuizz = this.allQuizz[oldID];
+        this.saveAnswer();
+        // clear format
+        this.clearFormats(this.currentQuizz.formats);
+        this.clearCurrentQuizz();        
+        // load history object
+        this.currentQuizz = {... this.allQuizz[oldID]}
+        this.oldQuizz = true;
+       // lg(this.currentQuizz);
+        lg(this.currentQuizz.formats)
+        // load quizz on page
+        this.loadQuizz(Q[oldID]);
+        this.liveUpdate();
 
-    setUserChoice = (uc) => {
-        //get user choice
-        this.data.currentQuizz.userChoice = [... uc];
-        this.data.currentQuizz.answered = true;
-        let uac = uc.filter((i) => {return this.data.currentQuizz.ans.indexOf(i) < 0;},this.data.currentQuizz.ans)
-        
-        if(uac.length>0){
-            for(let ck of uac) {                
-                this.data.currentQuizz.bg.push( { id:"btn"+ck, cl:'wrong' } );
+        // load format on page
+        if(!this.currentQuizz.answered){
+            if(this.quizzInfoOn === true){
+                document.querySelector('#quizzDetails').classList.add(hider);
             }
+            return
         }
-        // call next quizz
-        this.loadNewQuizz();
+        this.loadFormat(this.currentQuizz.formats);
+        document.querySelector('#quizzDetails').classList.remove(hider);
+        this.quizzInfoOn = true;
+       
     }
-
 }
 
 
 let tx;
 
+// STEPS FOR LOADING MCQ IN AN ERROR PROOF WAY
+/*
+    ....LOADING....
+1. Get quizz and load in current quizz object.
+2. Load formats and classes in the active format object
+3. load quizz on page
+4. apply formats and classes on page
+
+    ...ANALYSING ANSWER...
+1. Retrieve answer from gorm
+2. anlalyse user choice against correct answer
+3. update user data accordingly
+4. apply answer base formating on page
+5. fetch next question
+*/
+
+class ShowIt {
+    constructor(eventType, triggerId, targertId, targetDefaultDisplayNone ){
+        this.sID = [... triggerId];
+        this.tID = targertId;
+        this.eType = eventType;
+        this.displayNone = targetDefaultDisplayNone;
+        this.#bindAll();   
+    }
+
+    #bindIt = (theID) => {
+       
+        document.querySelector('#' + theID).addEventListener(this.eType, () => {
+            let elm = document.querySelector('#' + this.tID);
+            if(this.displayNone){
+                elm.classList.remove('hideit');
+                this.displayNone = false;
+                return
+            }            
+            elm.classList.add('hideit');
+            this.displayNone = true;
+        })
+       
+    }
+
+    #bindAll = () => {for(let id of this.sID){ this.#bindIt(id); }}
+}
+
+
 this.addEventListener('load', () => {
    
     tx = new Test();
+    let result = new ShowIt('click', ['showScore'], 'liveScore', true)
+    let qInfo = new ShowIt('click', ['openInfoWrapper','closeInfoWrapper'], 'infoWrapper', true)
     
     document.querySelector('#nextq').addEventListener('click', () => {
-        tx.loadNewQuizz();
+        tx.quizzManager();
+    })
+
+    document.querySelector('#startTest').addEventListener('click', () => {
+        tx.start();
     })
 
 })
